dcc3895a18e71879c1c25edee08a83cd92dee924
Masashi Hirosaki <m-hiro@nttpc.co.jp>
2017-11-25 12:51:17 +0000

    20171125-06

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   
 
-  devise_for :users
-  get 'top/index'
-
+ devise_for :users, controllers: {
+    registrations: "users/registrations",
+    omniauth_callbacks: "users/omniauth_callbacks"
+}
   resources :topics, only: [:index, :new, :create, :edit, :update, :destroy] 
   
   
